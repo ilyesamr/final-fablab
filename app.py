a32301f2249c5780bf7635eaa04a4a062cee4501
Gachita31 <44901156+Gachita31@users.noreply.github.com>
2020-06-09 15:03:51 +0200

    Models

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from db import db
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/fablab'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     @app.route('/')
     def home():
@@ -21,6 +26,4 @@ def create_app():
     def contact():
         return render_template('contact.html')
 
-    if __name__ == '__main__':
-        app.run()
-        app.debug = 1
+    return app
---
+++ models.py
@@ -0,0 +1,25 @@
+from db import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
