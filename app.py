3018ffd12d0b99b8271bfc4d58a65517498091f3
Allard Thibaut <thibaut.allard@viacesi.fr>
2020-06-11 11:04:35 +0200

    boutique

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fablab.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 from models import db
 
 
@@ -12,6 +13,17 @@ from models import db
 def home():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/boutique')
+def boutique():
+    return render_template('boutique.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
