35d68cfe1170532b330645d37ac883071ff48e5b
Gachita31 <44901156+Gachita31@users.noreply.github.com>
2020-06-12 13:43:11 +0200

    front

--- forms.py
+++ forms.py
@@ -4,21 +4,21 @@ from wtforms.validators import Email, Length, InputRequired, EqualTo, DataRequir
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Votre Email', validators=[InputRequired(), Length(min=0, max=20)])
-    password = PasswordField('Votre mot de passe', validators=[InputRequired(), Length(min=0, max=100)])
+    email = StringField('Email', validators=[InputRequired(), Length(min=0, max=20)])
+    password = PasswordField('Mot de passe', validators=[InputRequired(), Length(min=0, max=100)])
     remember = BooleanField('Se souvenir de moi')
     submit = SubmitField("Se connecter")
 
 
 class SignupForm(FlaskForm):
     """User Signup Form."""
-    name = StringField('Nom', validators=[InputRequired()])
-    location = StringField('Localisation', validators=[InputRequired(), Length(max=20)])
+    name = StringField('Nom', validators=[InputRequired()], render_kw={"placeholder": "Nom"})
+    location = StringField('Localisation', validators=[InputRequired(), Length(max=20)], render_kw={"placeholder": "Localisation"})
     email = StringField('Email',
-                        validators=[Length(max=100), Email(message='Entrer un email valide.'), InputRequired()])
+                        validators=[Length(max=100), Email(message='Entrer un email valide.'), InputRequired()], render_kw={"placeholder": "Email"})
     password = PasswordField('Mot de passe', validators=[InputRequired(), Length(min=8, max=100,
-                                                                             message='Veuillez mettre un mot de passe plus sécurisé')])
+                                                                             message='Veuillez mettre un mot de passe plus sécurisé')], render_kw={"placeholder": "Mot de passe"})
     confirm = PasswordField('Confirmer votre mot de passe', validators=[InputRequired(), EqualTo('password',
-                                                                                             message='les mots de passe doivent correspondre.')])
+                                                                                             message='les mots de passe doivent correspondre.')], render_kw={"placeholder": "Confirmation du mot de passe"})
 
     submit = SubmitField('Soumetre')
