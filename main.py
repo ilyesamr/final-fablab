58a72c6020ea186f1383af217180ef8584a6dd57
ilyes ameur <44901156+ilyesamr@users.noreply.github.com>
2020-07-03 11:16:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, fresh_login_required
 from werkzeug.security import generate_password_hash
 from app import db
 from models import Command, Product, User
-from forms import EditProfilForm
-
+from forms import EditProfilForm, EditPassword
 main = Blueprint('main', __name__)
 
 
@@ -20,11 +19,12 @@ def profil():
     form = EditProfilForm()
     achats = Command.query.filter(Command.user_id == current_user.id).all()
     achats_id = Command.product_id
-    products = Product.query.filter(Product.id == achats_id).all()
+    products = Product.query.filter(Product.id == Command.product_id).all()
     return render_template('profil.html', user=current_user, achats=achats, products=products, form=form)
 
 
 @main.route('/profil/<int:id>', methods=['POST'])
+@fresh_login_required
 @login_required
 def profil_post(id):
     form = EditProfilForm()
@@ -34,3 +34,19 @@ def profil_post(id):
     db.session.commit()
     flash("Votre compte a été mis a jour !")
     return redirect('/profil')
+
+
+@main.route('/security/<int:id>', methods=['GET', 'POST'])
+@login_required
+def security(id):
+    form = EditPassword()
+    user = User.query.get_or_404(id)
+    if request.method == 'POST' and form.validate_on_submit():
+        password = generate_password_hash(form.password.data, method='sha256')
+        user.password = password
+        db.session.commit()
+        flash("Votre compte a été mis a jour !")
+        return redirect('/profil')
+    else:
+        return render_template('security.html', user=user, form=form)
+
